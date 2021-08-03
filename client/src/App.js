b18dc43ad8ae0ba1ed1752afeef9c8c2d4f5d9ba
Tolga Secme <tolgasecme@icloud.com>
2021-08-03 12:00:57 -0600

    Client Side Setup is ready for the Auth

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { Container} from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import Auth from './Components/Auth/Auth';
+
+function App() {
+  const user = JSON.parse(localStorage.getItem('profile'));
+  
+  return (
+    
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Switch>
+          <Route exact path="/auth" component={() => (!user ? <Auth /> : <Redirect to="/" />)}></Route>
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Utils/API.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:3000' });
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    signIn: function (formData) {
+        return API.post('/users/signin', formData);
+    },
+    signUp: function (formData) {
+        return API.post('/users/signup', formData);
+    }
+};
