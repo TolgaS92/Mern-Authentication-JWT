4e4f413911421d2aed81d6cca969e8bbdddd2e41
Tolga Secme <tolgasecme@icloud.com>
2021-08-09 19:35:55 -0600

    Heroku Deployment

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Mern-Authentication-JWT
 
 Authentication boilerplate with react redux(+google sign in)
+
+Here's the link for this application:
+[Live Link for the Application](https://mern-authentication-jwt.herokuapp.com/)
--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mern-authentication-jwt.herokuapp.com' });
+const API = axios.create({ baseURL: 'http://localhost:3000' });
 /* https://mern-authentication-jwt.herokuapp.com */
 /* http://localhost:3000 */
 API.interceptors.request.use((req) => {
