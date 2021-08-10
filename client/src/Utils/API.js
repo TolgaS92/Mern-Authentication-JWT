dc3a949d7b9af30cc494dbd2bf06bc40e6af58b2
Tolga Secme <tolgasecme@icloud.com>
2021-08-09 19:30:23 -0600

    update on API call

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000' });
-
+const API = axios.create({ baseURL: 'https://mern-authentication-jwt.herokuapp.com' });
+/* https://mern-authentication-jwt.herokuapp.com */
+/* http://localhost:3000 */
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
